09e59ed95af3ab109bd3a8d1dff0c3941f5eeddc
lazymonkeycode <monkeycode23@ejemplo.com>
2025-06-01 18:02:29 -0300

    some fixes44

--- src/renderer/services/api.js
+++ src/renderer/services/api.js
@@ -19,8 +19,8 @@ export interface PagosPendientesResponse { ... }
 export interface PagosHistorialResponse { ... }
 */
 
-const API_BASE_URL = 'http://localhost:4000/api';
-// const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/api'; // Opción con variable de entorno
+const API_BASE_URL =process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
+// const API_BASE_URL =  'http://localhost:4000/api'; // Opción con variable de entorno
 
 const api = axios.create({
   baseURL: API_BASE_URL,
